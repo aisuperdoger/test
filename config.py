102f2477e1b4c0b8fdf4af2f218c6e76fb64c51f
onEpAth936 <63861767+onEpAth936@users.noreply.github.com>
2022-02-06 02:25:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,3 @@ import os
 #cookie中的koa:sess与koa:sess.sig参数
 KOA_SESS = os.environ['KOA_SESS'].strip()
 KOA_SESS_SIG = os.environ['KOA_SESS_SIG'].strip()
-SEND_KEY = os.environ['SEND_KEY'].strip()
-SERVER = os.environ['SERVER'].strip()
